bf8d255a750fa15ef75fcee966c76df627e46c81
Eric Mignot <eric.mignot@gmail.com>
2019-05-01 08:58:42 -0700

    Travis is GMT

--- demo/14.date.formating.spec.js
+++ demo/14.date.formating.spec.js
@@ -20,8 +20,12 @@ describe('Date formating', function() {
     it('is always a little plumbing', async ()=> {
         page = await HomePage(driver, port)
         if (process.env.YAF_SHOW_AND_WAIT) { await page.wait(15*60*1000) }
+        var sutUtcTime = 14
+        var offset = new Date().getTimezoneOffset() / 60
+        var localTime = 14 - offset
+        localTime = localTime < 10 ? '0'+localTime : ''+localTime
 
         expect(await page.text('#good-news-1-title')).to.equal('Gaining power')
-        expect(await page.text('#good-news-1-date')).to.equal('2019-05-01 07:33:14')
+        expect(await page.text('#good-news-1-date')).to.equal('2019-05-01 ' + localTime + ':33:14')
     })
 })
