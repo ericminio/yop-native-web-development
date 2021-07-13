97c28dafbe268d65db9a844d0cd0f35d461651b8
ericminio <eric.mignot@gmail.com>
2021-07-13 08:59:11 -0700

    better navigation

--- yop/lib/5.navigate.js
+++ yop/lib/5.navigate.js
@@ -1,8 +1,15 @@
 class Navigation {
 
+    constructor(window) {
+        this.window = window;
+        this.window.onpopstate = ()=> {
+            events.notify('navigation');
+        }
+    }
+
     to(target) {
         history.pushState({}, null, target);
-        events.notify('navigation')
+        events.notify('navigation');
     }
 }
-var navigate = new Navigation()
+var navigate = new Navigation(window);
