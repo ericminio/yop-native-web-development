eae38c0e95331a5cc1584ac177ecf51f33fbb74a
Eric Mignot <eric.mignot@gmail.com>
2021-07-14 15:08:53 -0700

    brag about store and events

--- yop/specs/7.brag.spec.js
+++ yop/specs/7.brag.spec.js
@@ -9,14 +9,14 @@ describe('Brag', ()=>{
         let html = `
             <html>
                 <body>
-                <when-not-authorized>
-                    <div id="alert">you need to sign in</div>
-                    <button id="signin">signin</button>
-                </when-not-authorized>
-                <when-authorized>
-                    <div id="content">welcome</div>
-                    <button id="signout">signout</button>
-                </when-authorized>
+                    <when-not-authorized>
+                        <div id="alert">you need to sign in</div>
+                        <button id="signin">signin</button>
+                    </when-not-authorized>
+                    <when-authorized>
+                        <div id="content">welcome</div>
+                        <button id="signout">signout</button>
+                    </when-authorized>
 
                     <script>${yop}</script>
                     <script>
@@ -88,6 +88,65 @@ describe('Brag', ()=>{
             button.click();
             expect(document.querySelector('when-not-authorized').innerHTML.trim()).to.equal('');
             expect(document.querySelector('when-authorized').innerHTML.trim()).not.to.equal('');
+        });
+    });
+    describe('About store and events', ()=>{
+        let html = `
+            <html>
+                <body>
+                    <system-status>
+                        <div id="status"></div>
+                    </system-status>
+
+                    <script>${yop}</script>
+                    <script>
+                        let api = {
+                            ping: ()=> {
+                                return new Promise((resolve, reject)=>{
+                                    resolve();
+                                })
+                            }
+                        }
+                        class SystemStatus extends HTMLElement {
+                            constructor() {
+                                super();
+                                this.count = 0;
+                            }
+                            connectedCallback() {
+                                events.register(this, 'interval set');
+                                store.save('interval', 5 * 60 * 1000);
+                                events.notify('interval set');
+                            }
+                            update(value, event) {
+                                this.interval = store.get('interval');
+                                if (this.intervalId !== undefined) {
+                                    clearInterval(this.intervalId);
+                                }
+                                this.intervalId = setInterval(()=>{ this.refresh(); }, this.interval);
+                            }
+                            refresh() {
+                                this.count ++;
+                                this.querySelector('#status').innerHTML = this.count;
+                            }
+                        }
+                        customElements.define('system-status', SystemStatus);
+                    </script>
+                </body>
+            </html>
+        `;
+        let window;
+        let document;
+        beforeEach(()=>{
+            window = new JSDOM(html, { url: 'https://localhost/hello-world', runScripts: 'dangerously' }).window;
+            document = window.document;
+        });
+        it('can be used to run a task regularly', (done)=>{
+            window.store.save('interval', 150);
+            window.events.notify('interval set');
+            setTimeout(()=>{
+                expect(document.querySelector('#status').innerHTML).to.equal('3');
+                done();
+            }, 500);
         });
     });
 });
